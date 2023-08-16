a9d93acf0a59ea4a7dd855fe1953dfa2e6074118
Kevin Molina <kevinmolina.dev@gmail.com>
2023-08-16 16:06:52 -0400

    agregando el componente header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import banner from "../../assets/header.png"
+const Header=()=>{
+    return(
+        <header>
+            <img src={banner} alt="header" />
+        </header>
+    )
+}
+export default Header;
